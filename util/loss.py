af80c08e66da83f603deab5adda4915736495b58
wrk1231 <wrk1231@gmail.com>
2019-10-04 00:11:25 -0400

    add utils-loss

--- util/loss.py
+++ util/loss.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 class LOSS(object):
-    def mean_square_error(self, x, target):
-        return 0.5 * np.square(target - x)
+    def mean_square_error(self, y, target):
+        return 0.5 * np.square(target - y)
 
-    def cross_entropy(self):
-        pass
+    def cross_entropy(self,y, target):
+        h = 1e-7
+        return -np.sum(target * np.log(y +h))
