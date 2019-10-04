6c3e3f829351705de5f95df419d0e6f8d4e98e01
wrk1231 <wrk1231@gmail.com>
2019-10-03 22:52:13 -0400

    add utils-loss

--- util/derivative.py
+++ util/derivative.py
@@ -3,8 +3,13 @@ import numpy as np
 class DERIVATIVE(object):
 
     def numerical_diff(self, func, x):
+        """
+
+        :param func: pass in a function to be differentiated
+        :param x: a sensible value 
+        :return:
+        """
 
         h = 1e-4
         return (func(x+h) - func(x-h)) / (2*h)
 
-    
