0c69f5c25bcee2739e315cd28ca1b32299655a49
wrk1231 <wrk1231@gmail.com>
2019-10-03 22:06:24 -0400

    add utils

---
+++ util/utilfunc.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+class UTIL(object):
+    def softmax(self, x: np.array) -> np.array:
+        """
+
+        :param x:
+        :return:
+        """
+        C = np.max(x)
+        return np.exp(x + C) / sum(np.exp(x + C))
