b390b8f3862c13ddec299b87de72652de3cf8a35
wrk1231 <wrk1231@gmail.com>
2019-10-16 22:34:46 -0400

    add gradient

--- util/derivative.py
+++ util/derivative.py
@@ -13,3 +13,25 @@ class DERIVATIVE(object):
         h = 1e-4
         return (func(x+h) - func(x-h)) / (2*h)
 
+    def numerical_gradient(self, func, x):
+        """
+
+        :param func: The function to calculate gradient
+        :param x: The point to calculate the gradient
+        :return:
+        """
+        h = 1e-4
+        grad = np.zeros_like(x)
+
+        for idx in range(x.size):
+            temp_val = x[idx]
+            x[idx] = temp_val + h
+            func_right = func(x)
+
+            x[idx] = temp_val - h
+            func_left = func(x)
+
+            grad[idx] = (func_right - func_left)/(2*h)
+            x[idx] = temp_val
+
+        return grad
